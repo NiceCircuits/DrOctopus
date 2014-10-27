828c3507727c5f5412da7796b6308ff66d521f21
NiceCircuits <74hc08@gmail.com>
2014-10-27 17:05:59 +0100

    .

--- soft/python/DrOctopus/inverseKinematics.py
+++ soft/python/DrOctopus/inverseKinematics.py
@@ -45,22 +45,27 @@ class inverseKinematics(object):
         self.Y = 0.0
         
     def moveTo(self, X, Y):
-        X=float(Y)
+        X=float(X)
         Y=float(Y)
         # Inverse kinematics
         L = math.sqrt(X*X+Y*Y) # length from (0,0) to (X,Y)
+        LtoLMax = L/(self.L1+self.L2)
+        if(LtoLMax >=1):
+            L = L / LtoLMax *0.999
+            X = X / LtoLMax *0.999
+            Y = Y / LtoLMax *0.999
         AL = math.atan2(Y, X) # angle between X axis and L
         ALL1 = math.acos((self.L1*self.L1 - self.L2*self.L2 + L*L)/2/self.L1/L) # angle between L1 and L
         self.A1 = AL + ALL1
         self.A2 = math.acos((self.L1*self.L1 + self.L2*self.L2 - L*L)/2/self.L1/self.L2)
         self.A3 = self.A1 + self.A2 - math.radians(90.0)
-        #Forward Kinematics
+        # Forward Kinematics - sanity check
         X1 = self.L1*math.cos(self.A1) #position of first joint
         Y1 = self.L1*math.sin(self.A1)
         A2abs = self.A1 + self.A2 -math.radians(180.0) #first angle - absolute, between X and L2 
         self.X = X1 + self.L2*math.cos(A2abs)
         self.Y = Y1 + self.L2*math.sin(A2abs)
-        self._X1=X1
+        self._X1=X1 # for debug purposes
         self._Y1=Y1
 
 
--- soft/python/DrOctopus/testInverseKinematics.py
+++ soft/python/DrOctopus/testInverseKinematics.py
@@ -5,9 +5,23 @@ Created on 27.10.2014
 '''
 from inverseKinematics import *
 
-
 if __name__ == '__main__':
     ik = inverseKinematics(1,1)
-    ik.moveTo(0.5, 0.5)
-    print([0, ik._X1, ik.X], [0, ik._Y1, ik.Y])
+#     ik.moveTo(1, 1)
+#     print [[ik._X1, ik._Y1], [ik.X, ik.Y]]
+#     print [ik.A1, ik.A2, ik.A3]
+#     ik.moveTo(1.4, 1.4)
+#     print [[ik._X1, ik._Y1], [ik.X, ik.Y]]
+#     print [ik.A1, ik.A2, ik.A3]
+#     ik.moveTo(2, 2)
+#     print [[ik._X1, ik._Y1], [ik.X, ik.Y]]
+#     print [ik.A1, ik.A2, ik.A3]
+#     ik.moveTo(1.6, 0.8)
+#     print [[ik._X1, ik._Y1], [ik.X, ik.Y]]
+#     print [ik.A1, ik.A2, ik.A3]
+    ik.moveTo(2, 1)
+    print [[ik._X1, ik._Y1], [ik.X, ik.Y]]
+    print [ik.A1, ik.A2, ik.A3]
+    ik.moveTo(1, 1)
+    print [[ik._X1, ik._Y1], [ik.X, ik.Y]]
     print [ik.A1, ik.A2, ik.A3]

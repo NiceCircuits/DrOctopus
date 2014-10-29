f324e61408bb7fb2985ec87e987a6e104fd9d155
NiceCircuits <74hc08@gmail.com>
2014-10-29 16:51:12 +0100

    .

--- soft/python/DrOctopus/inverseKinematics.py
+++ soft/python/DrOctopus/inverseKinematics.py
@@ -5,6 +5,8 @@ Created on 27.10.2014
 '''
 
 '''
+           A0 (horizontal plane), Z - axis ortogonal to X, Y
+           |
           ,-.  (0,0)
          ( / .......
           |-'
@@ -28,45 +30,57 @@ Created on 27.10.2014
 import math
 
 class inverseKinematics(object):
-    '''
-    classdocs
-    '''
 
     def __init__(self, L1, L2):
-        '''
-        Constructor
-        '''
         self.L1 = float(L1)
         self.L2 = float(L2)
-        self.A1 = 180.0
-        self.A2 = 180.0
-        self.A3 = 90.0
+        self.A0 = 0.0
+        self.A1 = math.radians(180.0)
+        self.A2 = math.radians(180.0)
+        self.A3 = math.radians(90.0)
         self.X = float(L1+L2)
         self.Y = 0.0
+        self.Z = 0.0
+        self.limits=[[self.X, self.X], [0.0,0.0], [0.0,0.0]]
         
-    def moveTo(self, X, Y):
-        X=float(X)
-        Y=float(Y)
-        # Inverse kinematics
-        L = math.sqrt(X*X+Y*Y) # length from (0,0) to (X,Y)
+    def limitTo(self, value, limits):
+        if (value<limits[0]):
+            return limits[0]
+        elif (value>limits[1]):
+            return limits[1]
+        else:
+            return value
+        
+    def moveTo(self, X, Y, Z):
+        X = self.limitTo(float(X), self.limits[0])
+        Y = self.limitTo(float(Y), self.limits[1])
+        Z = self.limitTo(float(Z), self.limits[2])
+        # inverse kinematics - rotate plane XY in Z axis
+        self.A0 = math.atan2(Z, X) # rotation in Z axis
+        # Inverse kinematics in rotated plane
+        Xrot = math.sqrt(X*X+Z*Z) 
+        L = math.sqrt(Xrot*Xrot+Y*Y) # length from (0,0) to (X,Y)
         LtoLMax = L/(self.L1+self.L2)
         if(LtoLMax >=1):
             L = L / LtoLMax *0.999
-            X = X / LtoLMax *0.999
+            Xrot = Xrot / LtoLMax *0.999
             Y = Y / LtoLMax *0.999
-        AL = math.atan2(Y, X) # angle between X axis and L
+        AL = math.atan2(Y, Xrot) # angle between X axis and L
         ALL1 = math.acos((self.L1*self.L1 - self.L2*self.L2 + L*L)/2/self.L1/L) # angle between L1 and L
         self.A1 = AL + ALL1
         self.A2 = math.acos((self.L1*self.L1 + self.L2*self.L2 - L*L)/2/self.L1/self.L2)
         self.A3 = self.A1 + self.A2 - math.radians(90.0)
         # Forward Kinematics - sanity check
-        X1 = self.L1*math.cos(self.A1) #position of first joint
+        X1rot = self.L1*math.cos(self.A1) #position of first joint
         Y1 = self.L1*math.sin(self.A1)
-        A2abs = self.A1 + self.A2 -math.radians(180.0) #first angle - absolute, between X and L2 
-        self.X = X1 + self.L2*math.cos(A2abs)
+        A2abs = self.A1 + self.A2 -math.radians(180.0) #first angle - absolute, between X and L2
+        Xrot = (X1rot + self.L2*math.cos(A2abs)) 
+        self.X = Xrot*math.cos(self.A0) 
         self.Y = Y1 + self.L2*math.sin(A2abs)
-        self._X1=X1 # for debug purposes
+        self.Z = Xrot*math.sin(self.A0)
+        self._X1=X1rot*math.cos(self.A0)  # for debug purposes
         self._Y1=Y1
+        
 
 
 
--- soft/python/DrOctopus/testInverseKinematics.py
+++ soft/python/DrOctopus/testInverseKinematics.py
@@ -7,21 +7,7 @@ from inverseKinematics import *
 
 if __name__ == '__main__':
     ik = inverseKinematics(1,1)
-#     ik.moveTo(1, 1)
-#     print [[ik._X1, ik._Y1], [ik.X, ik.Y]]
-#     print [ik.A1, ik.A2, ik.A3]
-#     ik.moveTo(1.4, 1.4)
-#     print [[ik._X1, ik._Y1], [ik.X, ik.Y]]
-#     print [ik.A1, ik.A2, ik.A3]
-#     ik.moveTo(2, 2)
-#     print [[ik._X1, ik._Y1], [ik.X, ik.Y]]
-#     print [ik.A1, ik.A2, ik.A3]
-#     ik.moveTo(1.6, 0.8)
-#     print [[ik._X1, ik._Y1], [ik.X, ik.Y]]
-#     print [ik.A1, ik.A2, ik.A3]
-    ik.moveTo(2, 1)
-    print [[ik._X1, ik._Y1], [ik.X, ik.Y]]
-    print [ik.A1, ik.A2, ik.A3]
-    ik.moveTo(1, 1)
-    print [[ik._X1, ik._Y1], [ik.X, ik.Y]]
-    print [ik.A1, ik.A2, ik.A3]
+    ik.moveTo(0, 1, -1)
+    print ["%0.3f"%f for f in [ik._X1, ik._Y1]]
+    print ["%0.3f"%f for f in [ik.X, ik.Y, ik.Z]]
+    print ["%0.3f"%f for f in[ik.A0, ik.A1, ik.A2, ik.A3]]

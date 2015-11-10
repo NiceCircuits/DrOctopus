fb3676956e41e21cb0239c503bff28f88a697855
NiceCircuits <piotr@nicecircuits.com>
2015-11-10 21:39:47 +0100

    First test software for MQTT for Python (Spyder) and Arduino (Eclipse).

---
+++ soft/drOctopusArduinoTest/drOctopusArduinoTest.ino
@@ -0,0 +1,108 @@
+/**
+ * \file
+ *       ESP8266 MQTT Bridge example
+ * \author
+ *       Tuan PM <tuanpm@live.com>
+ */
+#include <SoftwareSerial.h>
+#include <espduino.h>
+#include <mqtt.h>
+
+SoftwareSerial debugPort(A2, A3); // RX, TX
+ESP esp(&Serial, &Serial, 4);
+MQTT mqtt(&esp);
+boolean wifiConnected = false;
+
+void wifiCb(void* response)
+{
+  uint32_t status;
+  RESPONSE res(response);
+
+  if(res.getArgc() == 1) {
+    res.popArgs((uint8_t*)&status, 4);
+    if(status == STATION_GOT_IP) {
+      debugPort.println("Wifi connected");
+      //mqtt.connect("yourserver.com", 1883, false); // not needed - esp-link connects automatically
+      wifiConnected = true;
+      //or mqtt.connect("host", 1883); /*without security ssl*/
+    } else {
+      wifiConnected = false;
+      mqtt.disconnect();
+    }
+
+  }
+}
+
+void mqttConnected(void* response)
+{
+  debugPort.println("MQTT connected");
+  mqtt.subscribe("/drOctopus/test/#"); //or mqtt.subscribe("topic"); /*with qos = 0*/
+  mqtt.publish("/drOctopus/test/arduSend", "data0");
+
+}
+void mqttDisconnected(void* response)
+{
+
+}
+void mqttData(void* response)
+{
+  RESPONSE res(response);
+
+  debugPort.print("Received: topic=");
+  String topic = res.popString();
+  debugPort.println(topic);
+
+  debugPort.print("data=");
+  String data = res.popString();
+  debugPort.println(data);
+
+}
+void mqttPublished(void* response)
+{
+
+}
+void setup() {
+  Serial.begin(115200);
+  debugPort.begin(115200);
+  debugPort.println("ARDUINO: start");
+  esp.enable();
+  debugPort.println("ARDUINO: esp enable");
+  delay(500);
+  debugPort.println("ARDUINO: esp reset");
+  esp.reset();
+  delay(5000);
+  debugPort.println("ARDUINO: esp wait");
+  while(!esp.ready());
+
+  debugPort.println("ARDUINO: setup mqtt client");
+  if(!mqtt.begin("DVES_duino", "admin", "Isb_C4OGD4c3", 120, 1)) {
+    debugPort.println("ARDUINO: fail to setup mqtt");
+    while(1);
+  }
+
+
+  debugPort.println("ARDUINO: setup mqtt lwt");
+  mqtt.lwt("/lwt", "offline", 0, 0); //or mqtt.lwt("/lwt", "offline");
+
+/*setup mqtt events */
+  mqtt.connectedCb.attach(&mqttConnected);
+  mqtt.disconnectedCb.attach(&mqttDisconnected);
+  mqtt.publishedCb.attach(&mqttPublished);
+  mqtt.dataCb.attach(&mqttData);
+
+  /*setup wifi*/
+  debugPort.println("ARDUINO: setup wifi");
+  esp.wifiCb.attach(&wifiCb);
+
+  //esp.wifiConnect("DVES_HOME","wifipassword"); // not needed - esp-link connects automatically
+
+
+  debugPort.println("ARDUINO: system started");
+}
+
+void loop() {
+  esp.process();
+  if(wifiConnected) {
+
+  }
+}
---
+++ soft/drOctopusPythonTest/mqttTest.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 10 20:30:02 2015
+
+@author: piotr at nicecircuits.com
+"""
+
+# -*- coding: utf-8 -*-
+from __future__ import print_function # compatibility with python 2 and 3
+__author__ = 'piotr'
+import paho.mqtt.client as mqtt
+import serial, time, re, logging, numpy
+server = "test.mosquitto.org"
+port = 1883
+global received, ids, lastId, log, ser, client
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("/drOctopus/test/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    global received, ids, lastId, log
+    temp="topic: " + msg.topic + " " + "  message: " + str(msg.payload)
+    log.debug(temp)
+
+def kill_mqtt():
+    client.disconnect()
+    
+def tester_init():
+    global received, ids, lastId, log, ser, client
+    #enable debug logs
+    log=logging.getLogger()
+    log.setLevel(logging.DEBUG)
+    formatter=logging.Formatter('%(asctime)s - %(message)s')
+    logfile=logging.FileHandler("log.txt","w")
+    logfile.setLevel(logging.INFO)
+    logfile.setFormatter(formatter)
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    console.setFormatter(formatter)
+    log.addHandler(console)
+    log.addHandler(logfile)
+    
+    client = mqtt.Client(protocol=3)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect_async(server, port, 60)
+    
+    log.info("Application start")
+    client.loop_start()
+
+if __name__ =="__main__":
+    tester_init()
+    time.sleep(1)
+    for i in range(10):
+        client.publish("/drOctopus/test/test", "aaa")
+        time.sleep(1)

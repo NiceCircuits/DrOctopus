5d7adf08e989c90eee714fed8e839c9a4d1f5bb7
NiceCircuits <piotr@nicecircuits.com>
2015-11-13 19:39:22 +0100

    Add oled test

---
+++ soft/arduino_test/arduino_test_oled/.ino.cpp
@@ -0,0 +1,17 @@
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2015-11-13 19:31:07
+
+#include "Arduino.h"
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <nicer_Adafruit_SSD1306.h>
+void setup()   ;
+void testdrawchar(void) ;
+void testscrolltext(void) ;
+void loop() ;
+
+#include "arduino_test_oled.ino"
+

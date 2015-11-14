70dbe52158968f7764b4f3952069f607c8c91d2a
NiceCircuits <piotr@nicecircuits.com>
2015-11-14 09:49:27 +0100

    New, lighter OLED library test.

--- soft/arduino_test/arduino_test_oled/.ino.cpp
+++ soft/arduino_test/arduino_test_oled/.ino.cpp
@@ -1,17 +1,15 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2015-11-13 19:31:07
+//This file has been generated on 2015-11-13 22:33:18
 
 #include "Arduino.h"
-#include <SPI.h>
+#include <Arduino.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <nicer_Adafruit_SSD1306.h>
-void setup()   ;
-void testdrawchar(void) ;
-void testscrolltext(void) ;
-void loop() ;
+#include "U8glib.h"
+void draw(void) ;
+void setup(void) ;
+void loop(void) ;
 
 #include "arduino_test_oled.ino"
 
--- soft/arduino_test/arduino_test_oled/arduino_test_oled.ino
+++ soft/arduino_test/arduino_test_oled/arduino_test_oled.ino
@@ -1,111 +1,89 @@
-/*********************************************************************
-This is an example for our Monochrome OLEDs based on SSD1306 drivers
+/*
 
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/category/63_98
+  HelloWorld.pde
 
-This example is for a 128x64 size display using I2C to communicate
-3 pins are required to interface (2 I2C and one reset)
+  "Hello World!" example code.
 
-Adafruit invests time and resources providing this open source code,
-please support Adafruit and open-source hardware by purchasing
-products from Adafruit!
+  >>> Before compiling: Please remove comment from the constructor of the
+  >>> connected graphics display (see below).
 
-Written by Limor Fried/Ladyada  for Adafruit Industries.
-BSD license, check license.txt for more information
-All text above, and the splash screen must be included in any redistribution
-*********************************************************************/
+  Universal 8bit Graphics Library, https://github.com/olikraus/u8glib/
 
-#include <SPI.h>
+  Copyright (c) 2012, olikraus@gmail.com
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification,
+  are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this list
+    of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright notice, this
+    list of conditions and the following disclaimer in the documentation and/or other
+    materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+#include <Arduino.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <nicer_Adafruit_SSD1306.h>
+#include "U8glib.h"
 
-#define OLED_RESET -1
-Adafruit_SSD1306 display(OLED_RESET);
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE|U8G_I2C_OPT_DEV_0);	// I2C / TWI
 
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-
-#if (SSD1306_LCDHEIGHT != 64)
-#error("Height incorrect, please fix Adafruit_SSD1306.h!");
-#endif
-
-void setup()   {
-  Serial.begin(115200);
-
-  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
-  // init done
-
+void draw(void) {
+  // graphic commands to redraw the complete screen should be placed here
+  //u8g.setFont(u8g_font_unifont);
+  u8g.setFont(u8g_font_9x15);
+  u8g.drawStr( 0, 15, "netSSID");
+  u8g.setFont(u8g_font_8x13);
+  u8g.drawStr( 0, 31, "192.168.10.127");
+  u8g.drawStr( 0, 47, "test");
+  u8g.drawStr( 0, 63, "TEST12345");
 }
 
+void setup(void) {
+  // flip screen, if required
+  // u8g.setRot180();
 
-void testdrawchar(void) {
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
+  // set SPI backup if required
+  //u8g.setHardwareBackup(u8g_backup_avr_spi);
 
-  for (uint8_t i=0; i < 168; i++) {
-    if (i == '\n') continue;
-    display.write(i);
-    if ((i > 0) && (i % 21 == 0))
-      display.println();
-  }
-  display.display();
+  // assign default color value
+//	Wire.begin();
+//  if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
+//    u8g.setColorIndex(255);     // white
+//  }
+//  else if ( u8g.getMode() == U8G_MODE_GRAY2BIT ) {
+//    u8g.setColorIndex(3);         // max intensity
+//  }
+//  else if ( u8g.getMode() == U8G_MODE_BW ) {
+//    u8g.setColorIndex(1);         // pixel on
+//  }
+//  else if ( u8g.getMode() == U8G_MODE_HICOLOR ) {
+//    u8g.setHiColorByRGB(255,255,255);
+//  }
 }
 
-void testscrolltext(void) {
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(10,0);
-  display.clearDisplay();
-  display.println("scroll");
-  display.display();
+void loop(void) {
+  // picture loop
+  u8g.firstPage();
+  do {
+    draw();
+  } while( u8g.nextPage() );
 
-  display.startscrollright(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-  display.startscrollleft(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-  display.startscrolldiagright(0x00, 0x07);
-  delay(2000);
-  display.startscrolldiagleft(0x00, 0x07);
-  delay(2000);
-  display.stopscroll();
+  // rebuild the picture after some delay
+  delay(50);
 }
-
-void loop() {
-	  display.clearDisplay();
-
-	  // draw the first ~12 characters in the font
-	  testdrawchar();
-	  display.display();
-	  delay(2000);
-	  display.clearDisplay();
-
-	  // draw scrolling text
-	  testscrolltext();
-	  delay(2000);
-	  display.clearDisplay();
-
-	  // text display tests
-	  display.setTextSize(1);
-	  display.setTextColor(WHITE);
-	  display.setCursor(0,0);
-	  display.println("Hello, world!");
-	  display.setTextColor(BLACK, WHITE); // 'inverted' text
-	  display.println(3.141592);
-	  display.setTextSize(2);
-	  display.setTextColor(WHITE);
-	  display.print("0x"); display.println(0xDEADBEEF, HEX);
-	  display.display();
-	  delay(2000);
-
-}
-

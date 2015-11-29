82b2228f8a29cecf941a9989322a3dd50ea6c2f4
NiceCircuits <piotr@nicecircuits.com>
2015-11-29 11:17:55 +0100

    Add functions to synchronize time.

---
+++ soft/drOctopusArduino/time.h
@@ -0,0 +1,14 @@
+/*
+ * time.h
+ *
+ *  Created on: 2015-11-28
+ *      Author: Piotr Nowicki
+ */
+
+#ifndef TIME_H_
+#define TIME_H_
+
+uint32_t timeGet();
+void setTime(uint32_t newTime);
+
+#endif /* TIME_H_ */

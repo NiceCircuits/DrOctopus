e61b641948415ed4488695280d14f1ff1fc30586
NiceCircuits <piotr@nicecircuits.com>
2016-02-08 22:04:41 +0100

    Add sysTick module with delayMs function.

---
+++ soft/drOctopusStm32/src/sysTick.c
@@ -0,0 +1,32 @@
+/**
+ ******************************************************************************
+ * @file    sysTick.c
+ * @author  piotr@nicecircuits.com
+ * @date    2016-02-08
+ * @brief   System clock functions
+ ******************************************************************************
+ */
+
+#include "sysTick.h"
+#include "config.h"
+#include "stm32f30x.h"
+
+/**
+ * Store time elapsed from start (in ms). 32bit gives only ~49 days so 64bit
+ * is used - 580*10^6 years should be enough :)
+ */
+volatile uint64_t millisFromStart = 0;
+
+uint8_t sysTickInit(void) {
+	return SysTick_Config(SystemCoreClock / 1000);
+}
+
+void SysTick_Handler() {
+	millisFromStart++;
+}
+
+void delayMs(uint32_t time) {
+	uint64_t end = millisFromStart + time;
+	while (millisFromStart < end) {
+	}
+}
---
+++ soft/drOctopusStm32/src/sysTick.h
@@ -0,0 +1,26 @@
+/**
+  ******************************************************************************
+  * @file    sysTick.h
+  * @author  piotr@nicecircuits.com
+  * @date    2016-02-08
+  * @brief   System clock functions
+  ******************************************************************************
+*/
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+#include <inttypes.h>
+
+/**
+ * Initialize SysTick to generate exception every 1ms
+ * @return 0 if OK
+ */
+uint8_t sysTickInit(void);
+
+/**
+ * Delay for some ms.
+ * @param time Time in ms.
+ */
+void delayMs(uint32_t time);
+
+#endif /* SYSTICK_H_ */
